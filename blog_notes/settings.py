8f4e37ca0dba7d08990ef86d36ba08ebdd799683
andreynetrebin <164737630+andreynetrebin@users.noreply.github.com>
2024-09-24 00:09:39 +0300

    Update settings.py

--- blog_notes/settings.py
+++ blog_notes/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-k=c0p!n-(eyw!4=@$(+9u!nl_q^191^ii-wn=-om*mexloz@#$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 SITE_ID = 1
 # Application definition
@@ -78,12 +78,22 @@ WSGI_APPLICATION = 'blog_notes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+ DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.mysql',
+          'NAME': 'andreinetrebin89$default',
+          'USER': 'andreinetrebin89',
+          'PASSWORD': '',
+          'HOST': 'andreinetrebin89.mysql.pythonanywhere-services.com',
+      }
+ }
 
 AUTHENTICATION_BACKENDS = (
     # 'django.contrib.auth.backends.ModelBackend',
@@ -116,10 +126,10 @@ LOGIN_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
 
 # this code
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
